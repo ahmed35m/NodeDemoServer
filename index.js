4770a6962f386f81078ecf7009907ac6c48445ec
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-10-03 20:03:16 -0600

    API call demo added

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const http  = require('http')
-const path = require('path')
-const fs = require('fs')
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    if (req.url === '/'){
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+    //res.writeHead(200,{'Content-Type':'text/html'})
+    //res.end('<h1>home</h1>');
+  }
 
-        fs.readFile(path.join(__dirname,'public','index.html'),
-        (err,content)=>{
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "Ali", age: "12" },
+      { name: "John", age: "21" },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
 
-            if(err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.end(content)
-        })
-        //res.writeHead(200,{'Content-Type':'text/html'})
-        //res.end('<h1>home</h1>');
-    }
-})
-
-const PORT = process.env.PORT|| 5000;
-server.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
